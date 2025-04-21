856a2a00aa83d0e72436b3c4d0d723b7fd119415
TAN JIA LIN <125891820+JLtan1024@users.noreply.github.com>
2025-04-22 03:53:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+import asyncio
+import sys
+
+if sys.platform == "win32" and (3, 8, 0) <= sys.version_info < (3, 9, 0):
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    
 import streamlit as st
 from ultralytics import YOLO
 from PIL import Image, ImageDraw, ImageFont
